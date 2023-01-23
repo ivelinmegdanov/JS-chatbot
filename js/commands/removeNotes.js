79c09faff9a1272a8db8bfa78c604450999b79d4
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2023-01-23 23:51:10 +0200

    Reworked remove notes command

--- js/commands/removeNotes.js
+++ js/commands/removeNotes.js
@@ -1,22 +1,28 @@
 function removeNotes(userInput) {
-  var inputArray = userInput.split("-");
-  var list = inputArray[0].slice(11).trim();
-  var note = inputArray[1].trim();
-  var existing_notes = JSON.parse(localStorage.getItem("Tasks")) || {};
-  var sublists = list.split(" ");
-  var current_ref = existing_notes;
-  for (var i = 0; i < sublists.length; i++) {
+  const inputArray = userInput.split("-");
+  let list = inputArray[0].slice(11).trim();
+  let note = inputArray[1].trim();
+
+  const existing_notes = JSON.parse(localStorage.getItem("Tasks")) || {};
+
+  const sublists = list.split(" ");
+  let current_ref = existing_notes;
+
+  for (let i = 0; i < sublists.length; i++) {
     if (!current_ref[sublists[i]]) {
       response.innerHTML += "<p>Абе не намирам тая категория</p>";
       return;
     }
     current_ref = current_ref[sublists[i]];
   }
+
   if (!current_ref.notes) {
     response.innerHTML += "<p>Ти мръдна ли? Няма такава бележка в тая категория..</p>";
     return;
   }
-  var index = current_ref.notes.indexOf(note);
+
+  let index = current_ref.notes.indexOf(note);
+  
   if (index !== -1) {
     current_ref.notes.splice(index, 1);
     localStorage.setItem("Tasks", JSON.stringify(existing_notes));
