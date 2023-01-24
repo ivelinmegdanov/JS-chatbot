834c97c08527fced3e2b70ad60450b39002a90c0
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2023-01-24 17:22:45 +0200

    returnWeather now working

--- js/commands/returnWeather.js
+++ js/commands/returnWeather.js
@@ -1,4 +1,4 @@
-const API_KEY = "7edd01932dea97afedb8cc2292c7695e";
+const API_KEY = "1a260e62e35f609f529a1b306a7d3e0b";
 const city = "Varna";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
@@ -6,8 +6,27 @@ function returnWeather() {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = data.weather[0].description;
-      response.innerHTML += `<p>Chatbot: The weather in ${city} is ${weather}</p>`;
+      const windDirection = data.wind.deg;
+      const windSpeed = data.wind.speed;
+      const weatherDescription = data.weather[0].description;
+      const temp = Math.round(data.main.temp - 273.15);
+
+      console.log(data);
+
+      weatherResponses = [
+        {text: `Температурата е ${temp} градуса.`, chance: 1},
+        {text: `В момента е ${weatherDescription}.`, chance: 1},
+        {text: `За тези, които ги мързи да погледнат през прозореца - вън е ${weatherDescription}.`, chance: 0.2},
+        {text: `(Наплюнчва пръст и го показва през прозореца) Усещам вятър в посока ${windDirection}, около ${windSpeed} метра в секунда.`, chance: 1}
+      ];
+
+      if (temp < 0) {
+        weatherResponses.push({text: `Вън е ${temp} градуса, егати студа!`, chance: 1});
+      } else if (temp > 30) {
+        weatherResponses.push({text: `${temp} градуса, как живеете вие в тая жега, добре че съм бот!`, chance: 1});
+      }
+
+      response.innerHTML += `<p>${getRandomResponse(weatherResponses)}</p>`;
     })
     .catch((error) => {
       console.log("Error:", error);
