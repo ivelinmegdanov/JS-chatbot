1276d70bdbac76f00ff85a96f3a07c23b1052f2a
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2023-01-27 17:51:20 +0200

    Checking if the user input is valid

--- js/commands/takeNotes.js
+++ js/commands/takeNotes.js
@@ -6,31 +6,36 @@ function takeNotes(userInput) {
   //input
   const inputArray = userInput.split("-");
   const list = inputArray[0].slice(16).trim();
-  const note = inputArray[1].trim();
-  const existing_notes = JSON.parse(localStorage.getItem("Tasks")) || {};
-  const sublists = list.split(" ");
+  if (inputArray[1]){
+    const note = inputArray[1].trim();
+    const existing_notes = JSON.parse(localStorage.getItem("Tasks")) || {};
+    const sublists = list.split(" ");
   
-  let current_ref = existing_notes;
+    let current_ref = existing_notes;
 
-  for (let i = 0; i < sublists.length; i++) {
-    if (!current_ref[sublists[i]]) {
-      current_ref[sublists[i]] = {};
+    for (let i = 0; i < sublists.length; i++) {
+      if (!current_ref[sublists[i]]) {
+        current_ref[sublists[i]] = {};
+      }
+      current_ref = current_ref[sublists[i]];
     }
-    current_ref = current_ref[sublists[i]];
+
+    current_ref.notes = current_ref.notes ? current_ref.notes : [];
+    if(current_ref.notes.indexOf(note) === -1) {
+      current_ref.notes.push(note);
+
+      // troll note with a very low chance
+      if (randomNum < trollChance) {
+        current_ref.notes.push(getRandomResponse(trollNote));
+      }
+
+      response.innerHTML += `<p>Ш запомня не се бой :)</p>`;
+      localStorage.setItem("Tasks", JSON.stringify(existing_notes));
+    } else {
+      response.innerHTML += `<p>Вече имаш такава бележка в тази категория..</p>`;
+    }
   }
-
-  current_ref.notes = current_ref.notes ? current_ref.notes : [];
-  if(current_ref.notes.indexOf(note) === -1) {
-    current_ref.notes.push(note);
-
-    // troll note with a very low chance
-    if (randomNum < trollChance) {
-      current_ref.notes.push(getRandomResponse(trollNote));
-    }
-
-    response.innerHTML += `<p>Ш запомня не се бой :)</p>`;
-    localStorage.setItem("Tasks", JSON.stringify(existing_notes));
-  } else {
-    response.innerHTML += `<p>Вече имаш такава бележка в тази категория..</p>`;
+  else {
+    response.innerHTML += `<p>То хубаво ама ама каква да е бележката..</p>`;
   }
 }
