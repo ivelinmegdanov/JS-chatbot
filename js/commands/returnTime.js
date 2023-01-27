949a6de3f088f2dd40111990fcfae743ef0c23bf
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2023-01-27 18:33:21 +0200

    Added more time responses

--- js/commands/returnTime.js
+++ js/commands/returnTime.js
@@ -16,9 +16,19 @@ function returnTime() {
     const timeResponse = [
         { text: `абе май е ${formattedTime}.`, chance: 0.5 },
         { text: "абе май е 12:45.", chance: 0.1 },
-        { text: `Шес бес десет, няма бе, ${formattedTime} е...`, chance: 0.5 },
         { text: `Ако умножиш ${x} по ${y} ще получиш unix timestamp, който лесно можеш сам да си пресметнеш и да получиш текущата дата.`, chance: 0.1 },
     ];
+
+    if(hours >= 18) {
+        timeResponse.push({text: `Шес бес десет мина бе, ${formattedTime} е...`, chance: 0.5});
+    } else {
+        timeResponse.push({text: `Шес бес десет е. Няма бе ${formattedTime} е...`, chance: 0.5});
+    }
+
+    if(hours === 17 && minutes === 50){
+        response.innerHTML += `<p>Шес бес десет е!</p>`;
+    }
+    
 
     response.innerHTML += `<p>${getRandomResponse(timeResponse)}</p>`;
 }
